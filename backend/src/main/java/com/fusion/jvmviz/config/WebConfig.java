358a40e96c00968b7df9593f6585b3906f70ec1d
Abhishek Rathod <abhishekrathod5455@gmail.com>
2026-05-28 23:28:06 +0530

    Cors Enabled

--- backend/src/main/java/com/fusion/jvmviz/config/WebConfig.java
+++ backend/src/main/java/com/fusion/jvmviz/config/WebConfig.java
@@ -1,4 +1,16 @@
 package com.fusion.jvmviz.config;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5173"
+                ).allowedMethods("*");
+    }
 }
